b1359cf0242d1479387e1534751b4734e40a2993
Devin Ludwig and Haley Swain <devin@>
2016-12-14 16:29:10 -0800

    add routing to forms for adding to a recipe on recipe page.

--- app.rb
+++ app.rb
@@ -17,3 +17,17 @@ get('/recipe/:id') do
   @recipe = Recipe.find(params.fetch('id'))
   erb(:recipe_page)
 end
+
+post('/new_ingredient/:id') do
+  @recipe = Recipe.find(params.fetch('id'))
+  ingredient = Ingredient.create({:text => params.fetch("new_ingredient")})
+  @recipe.ingredients.push(ingredient)
+  Measurement.find_by({:recipe_id => @recipe.id, :ingredient_id => ingredient.id}).update({:amount => params.fetch('new_amount')})
+  redirect to ("/recipe/#{@recipe.id}")
+end
+
+post('/new_instruction/:id') do
+  @recipe = Recipe.find(params.fetch('id'))
+  @recipe.instructions.push(Instruction.create({:text => params.fetch("new_instruction")}))
+  redirect to ("/recipe/#{@recipe.id}")
+end
--- lib/instruction.rb
+++ lib/instruction.rb
@@ -1,4 +1,6 @@
 class Instruction < ActiveRecord::Base
   has_many :instructions_recipes
   has_many :recipes, through: :instructions_recipes
+  validates(:name, {:presence => true})
+  validates(:name, {:uniqueness => true})
 end
